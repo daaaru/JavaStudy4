ff97ec0abe8b365b4ac8bf046ef9f4805a0cef62
Jeonjueun07 <lovesj2323@naver.com>
2022-01-07 09:33:40 +0900

    Study

--- src/com/daru/s5/MyGod.java
+++ src/com/daru/s5/MyGod.java
@@ -3,13 +3,25 @@ package com.daru.s5;
 public class MyGod {
 //신을 한명만 만들고 싶음
 
-	private static MyGod myGod = new MyGod();
-
+//	private static MyGod myGod = new MyGod();
+//
+//	private MyGod() {}
+//	
+//	public static MyGod getInstance() {
+//		return myGod;
+//	}
+	private static MyGod myGod;
+	
 	private MyGod() {}
 	
-	public static MyGod getInstance() {
-		return myGod;
-	}
 	
+	public static MyGod makeGod() {
+		
+		if(MyGod.myGod == null) {
+			MyGod.myGod = new MyGod();
+		}
+		
+		return MyGod.myGod;
+	}
 
 }
--- src/com/daru/s5/MyGodMain.java
+++ src/com/daru/s5/MyGodMain.java
@@ -4,12 +4,14 @@ public class MyGodMain {
 
 	public static void main(String[] args) {
 		
-		MyGod myGod;
+		MyGod myGod = MyGod.makeGod();
 		
 		System.out.println(myGod);
 		//singleton
 		
-		
+		MyGod my2 = MyGod.makeGod();
+		System.out.println(my2);
+		System.out.println(MyGod.makeGod());
 		
 	}
 
