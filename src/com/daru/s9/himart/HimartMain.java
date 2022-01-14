23e3a3048778d55dd9f62b715bf03898223b545b
Jeonjueun07 <lovesj2323@naver.com>
2022-01-14 23:17:29 +0900

    Study

--- src/com/daru/s9/himart/HimartMain.java
+++ src/com/daru/s9/himart/HimartMain.java
@@ -1,9 +1,17 @@
 package com.daru.s9.himart;
 
+//import com.daru.s8.*.*; 패키지명이 중복되면안되므로 패키지명은 다써야함
+import com.daru.s8.rpg.*;
+
 public class HimartMain {
 
 	public static void main(String[] args) {
+		
 		//Tv, Coumputer, Phone 정보입력
+		
+		
+		Knight knight;
+		Archer archer;
 		
 		Tv tv = new Tv();
 		tv.company = "Lg";
